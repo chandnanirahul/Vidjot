b5e520178ab4454e30543199980b2079b8cf2342
Rahul chandnani <chandnanirahul40@gmail.com>
2018-05-03 13:39:55 +0530

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://rahul chandnani:A8085832101b@ds163689.mlab.com:63689/vidjot-prod'}
+  module.exports = {mongoURI: 'mongodb://rahul chandnani:A@8085832101b@ds113640.mlab.com:13640/vidjot-prod'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
